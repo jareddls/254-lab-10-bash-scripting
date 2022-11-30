53e588ff26bf5a17002afb85425400a48256a90a
Omar Trejo <54593489+omartrejo562@users.noreply.github.com>
2022-11-30 07:38:46 -0800

    Update question11_solution.sh
    Added BASH script

--- question11_solution.sh
+++ question11_solution.sh
@@ -1,1 +1,21 @@
+#! usr/bin/bash
 
+echo Directories
+
+for item in *
+do
+        if [ -d $item ]
+          then
+                echo $item
+        fi
+done
+
+echo files
+
+for item in *
+do
+        if [ -f $item ]
+          then
+                echo $item
+        fi
+done
